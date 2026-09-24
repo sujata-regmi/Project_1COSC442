34f9b415eeb4b4dffc317ef614b61177d015088e
sujata_regmi <sujuregmi27@gmail.com>
2026-09-23 20:37:29 -0400

    Task 11 – Extract local variable colorGroup from cell.getColorGroup in GameBoard.java

--- src/edu/towson/cis/cosc442/project1/monopoly/GameBoard.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/GameBoard.java
@@ -29,7 +29,8 @@ public class GameBoard {
 	
 	public void addCell(PropertyCell cell) {
 		int propertyNumber = getPropertyNumberForColor(cell.getColorGroup());
-		colorGroups.put(cell.getColorGroup(), new Integer(propertyNumber + 1));
+		String colorGroup = cell.getColorGroup();
+		colorGroups.put(colorGroup, new Integer(propertyNumber + 1));
         cells.add(cell);
 	}
 
